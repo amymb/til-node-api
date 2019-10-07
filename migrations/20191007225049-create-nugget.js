118415677e7e3e41a0cf88c25a01b9a7485588e9
Amy Bertken <amy.bertken@gmail.com>
2019-10-07 17:10:15 -0600

    add nuggets model and migration

---
+++ migrations/20191007225049-create-nugget.js
@@ -0,0 +1,42 @@
+'use strict';
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('Nuggets', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      title: {
+        type: Sequelize.STRING
+      },
+      description: {
+        type: Sequelize.STRING
+      },
+      externalLink: {
+        type: Sequelize.STRING
+      },
+      userId: {
+        type: Sequelize.INTEGER,
+        references: {
+          model: {
+            tableName: 'users'
+          },
+          key: 'id'
+        }
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('Nuggets');
+  }
+};
---
+++ models/nugget.js
@@ -0,0 +1,13 @@
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+  const Nugget = sequelize.define('Nugget', {
+    title: DataTypes.STRING,
+    description: DataTypes.STRING,
+    externalLink: DataTypes.STRING,
+    userId: DataTypes.INTEGER
+  }, {});
+  Nugget.associate = function(models) {
+    Nugget.belongsTo(models.User, {foreignKey: 'userId', as: 'user' })
+  };
+  return Nugget;
+};
